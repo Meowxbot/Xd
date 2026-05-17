a58de12081e97f0e940430e1294fa9f9b5dfb4f0
Meowxbot <276385678+Meowxbot@users.noreply.github.com>
2026-05-17 21:32:58 +0530

    Update userbot.py
    Hmmmm

--- SANYAMUSIC/core/userbot.py
+++ SANYAMUSIC/core/userbot.py
@@ -61,8 +61,7 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("urstarkz")
-                await self.one.join_chat("urstarkz")
+                await self.one.join_chat("darkquid")
             except:
                 pass
             assistants.append(1)
@@ -82,8 +81,7 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("ursstarkz")
-                await self.one.join_chat("itfuntimeXD")
+                await self.two.join_chat("darkquid")
             except:
                 pass
             assistants.append(2)
@@ -103,8 +101,7 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("ursstarkz")
-                await self.one.join_chat("itfuntimeXD")
+                await self.three.join_chat("darkquid")
             except:
                 pass
             assistants.append(3)
@@ -124,8 +121,7 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("ursstarkz")
-                await self.one.join_chat("itfuntimeXD")
+                await self.four.join_chat("darkquid")
             except:
                 pass
             assistants.append(4)
@@ -145,8 +141,7 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("ursstarkz")
-                await self.one.join_chat("itfuntimeXD")
+                await self.five.join_chat("darkquid")
             except:
                 pass
             assistants.append(5)
